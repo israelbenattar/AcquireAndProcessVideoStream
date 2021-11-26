f5d1b2f8f5891810e349f411843647a85cf1de78
israelbenattar <isrealbenattar@gmail.com>
2021-11-26 16:52:30 +0200

    StreamCapture updated and added geters

--- StreamCapture.cpp
+++ StreamCapture.cpp
@@ -10,20 +10,26 @@ using namespace std;
 class StreamCapture
 {
 private:
-    queue<Mat*> frames; 
-
-public:
-VideoCapture cap;
+    Mat* currFrame; 
+    queue<Mat> frames;
+    pthread_mutex_t frameLock;
+    bool eof;
+    VideoCapture cap;
     int frameWidth;
     int frameHeight;
-    StreamCapture(String video_path);
+    double fps;
+
+public:
+    StreamCapture(string video_path);
     ~StreamCapture();
     void start();
     static void* readFrame(void* arg);
-    Mat* getFrame();
+    Mat getFrame();
     bool empty();
-    bool EFO;
-    atomic<bool> readed;
+    bool endOfFile();
+    int getHeight();
+    int getWidth();
+    double getFps();
 };
 
 
@@ -32,21 +38,21 @@ VideoCapture cap;
  * 
  * @param video_path A path to a video file
  */
-StreamCapture::StreamCapture(String video_path)
+StreamCapture::StreamCapture(string video_path)
 {
     cap = VideoCapture(video_path);
 
-    frameWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-    frameHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
     //Checks if the file was opened successfully
     if(!cap.isOpened()){
         cerr << "Error: Cannot open video stream or file" << endl;
         exit(1);
     }
 
-    EFO = false;
-    readed = false;
-
+    // frames = new queue<Mat>;
+    fps = cap.get(CAP_PROP_FPS);
+    frameWidth = cap.get(CAP_PROP_FRAME_WIDTH);
+    frameHeight = cap.get(CAP_PROP_FRAME_HEIGHT);
+    eof = false;
 }
 
 
@@ -59,29 +65,29 @@ StreamCapture::~StreamCapture()
 
 
 /**
- * @brief Read the video stream frame by frame
+ * @brief Read the video stream until the end of the stream in const intarvel of 40ms
  * 
  * @param ptr pointer to a StreamCapture object
  * @return void* return NULL
  */
 void* StreamCapture::readFrame(void* ptr)
 {
+    int k = 0;
     StreamCapture* sc = (StreamCapture*) ptr;
     while (true)
     {
         Mat frame;
         sc->cap >> frame;
-
-        //Checks if end of stream
         if (frame.empty())
         {
-            sc->EFO = true; 
+            sc->eof = true; 
             break;
         }
-        
-        sc->frames.push(&frame);
-        // cout << frame.size() << endl;
-        usleep(40);
+        pthread_mutex_lock(&sc->frameLock);
+        sc->frames.push(frame);
+        pthread_mutex_unlock(&sc->frameLock);
+    
+        usleep(40 * 1000);
 
     }
     
@@ -105,15 +111,60 @@ void StreamCapture::start()
     
 }
 
-Mat* StreamCapture::getFrame()
+/**
+ * @brief Get the next frame from frames
+ * 
+ * @return Mat The frame if avilable
+ */
+Mat StreamCapture::getFrame()
 {
-    readed = true;
-    Mat* frame = frames.front();
+    pthread_mutex_lock(&frameLock);
+    Mat f = frames.front();
     frames.pop();
-    return frame;
+    pthread_mutex_unlock(&frameLock);
+    return f;
 }
 
+/**
+ * @brief checks if the fraem queue is empty
+ * 
+ * @return true if the freams queue is empty, else false
+ */
 bool StreamCapture::empty()
 {
     return frames.empty();
+}
+
+/**
+ * @brief checks if the stream is ended
+ * 
+ * @return true if the stream is ended, else false
+ */
+bool StreamCapture::endOfFile()
+{
+    return eof;
+}
+
+/**
+ * @return int the height of the stream fream
+ */
+int StreamCapture::getHeight()
+{
+    return frameHeight;
+}
+
+/**
+ * @return int the width of the stream fream
+ */
+int StreamCapture::getWidth()
+{
+    return frameWidth;
+}
+
+/**
+ * @return double the fream par second rate of the stream
+ */
+double StreamCapture::getFps()
+{
+    return fps;
 }
